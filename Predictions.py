4eff6c0f570d86c10599e1d22e446f2186420444
ana-mali <92402688+ana-mali@users.noreply.github.com>
2022-07-18 22:03:20 -0400

    Update Predictions.py

--- Predictions.py
+++ Predictions.py
@@ -1,97 +1,77 @@
 '''
 ----------------------------------------
-[Algorithm to predict hypothesis results]
+[Modeling Module]
 Anastasia & Kiran
 ----------------------------------------
-__updated__= "2022-07-17"
+__updated__= "2022-07-15"
 ----------------------------------------
 '''
-
-import pandas as pd 
+import pandas as pd #pip install pandas in command prompt
+#also install openpyxl 
 import matplotlib.pyplot as matplot
+data=pd.read_excel('CarInfo.xlsx',sheet_name='Sheet1') #read file into Dataframe
 
-data=pd.read_excel('CarInfo.xlsx',sheet_name='Sheet1') 
-ROWS=len(data.index)
-print('Number of rows: '+str(ROWS))
+#MODELING
+#_____________Year vs. Price___________________
+model1=data[data['Model']=='Jetta'] 
+model1=model1.sort_values(by=['Year'])
+model1=model1.loc[:,['Year','HP','Engine Size','Price']]
+bargraph=model1.plot.bar(x='Year',y='Price',color='red',fontsize='10')
+print('Sub-Dataframe including all Volkswagen Jetta')
+print(model1) #print sub-dataframe from main Dataframe
+matplot.show() #show first model regarding year vs. price of the Jetta
+#must close this graph in order to see the next one 
+#_____________Year vs. HorsePower_______________
+bargraph=model1.plot.bar(x='Year',y='HP')
+matplot.show()
 
-#______Average price of full dataset____
-avg_price=data.loc[:,['Price']]
-avg_p=0
-for x in avg_price.iterrows():#iterate through rows
-    avg_p+=x[1]
-    
-avg_p=avg_p/ROWS
-print("Average price:"+str(avg_p))
+#_____________Vintage cars_______________________
+#CAMARO
+model2=data[data['Model']=='Camaro ZL1']
+model2=model2.sort_values(by=["Year"])
+model2=model2.loc[:,["Year",'HP','Engine Size','Seats','Price','Cylinders']]
+print('Sub-Dataframe including all Chevrolet Camaro ZL1')
+bargraph=model2.plot.bar(x='Year',y='Price')
+matplot.show()
+print(model2)
+#FERRARI
+model3=data[data['Make']=='Ferrari']
+model3=model3.sort_values(by='Year')
+model3=model3.loc[:,['Model',"Year",'HP','Engine Size','Price','Cylinders','Transmission']]
+print("Sub-Dataframe including all Ferrari's")
+print(model3) 
+#______________Electric Cars______________
+model4=data[data['Electric']=='Yes']
+model4=model4.sort_values(by='Price')
+model4=model4.loc[:,['Model','HP','Price','Transmission','Body Style']]
+print("Sub-Dataframe including all electric cars")
+print(model4)
 
-#______Average horsepower of full dataset______
-avg_horsepower=data.loc[:,["HP"]]
-avg_hp=0
-for x in avg_horsepower.iterrows():
-    avg_hp+=x[1]
-avg_hp=avg_hp/ROWS 
-print("Average horsepower: "+str(avg_hp))
-
-#____Average price of each body style_____
-avg_bodystyle=[] #array of all averages 
-avg=0
-bodydataframe=data.loc[:,['Body Style','Price']]
+#_____________Price vs. Body Styles__________
 for x in data['Body Style'].unique():
-    temp=bodydataframe[bodydataframe['Body Style']==x]
-    for y in temp.iterrows():
-        a=y[1].to_list()
-        avg+=a[1]
-    avg=avg/ROWS 
-    avg=float("{:.2f}".format(avg))
-    temp1=[]
-    temp1.append(x) #name of body stule
-    temp1.append(avg) #average price within bodystyle
-    avg_bodystyle.append(temp1)
+    print('Sub-Dataframe including all '+x+' cars')
+    model=data[data['Body Style']==x]
+    model=model.sort_values(by='Price')
+    model=model.loc[:,['Model','Price','HP','Doors','Seats','Year']]
+    print(model)
 
-print('list of average bodystyle prices:')
-for x in avg_bodystyle:
-    print(x)
-#percentage difference compared to price average
-percents=[]
-for x in avg_bodystyle:
-    temp1=[]
-    per=x[1]/avg_p 
-    temp1.append(x[0])
-    temp1.append(per)
-    percents.append(temp1)
-
-print('percent difference within each bodystyle:')
-print(percents)
-
-#______Average Horsepower______
-avg_hp=0
-horsepower=data.loc[:,['HP','Price']]
-for x in horsepower.iterrows():
-    avg_hp+=x[1] 
-print(avg_hp)
-avg_hp=avg_hp/ROWS #calculate avg horsepower of whole dataset
-print("Average horsepower: "+str(avg_hp))
-
-#______Average price within horsepower interval________
-avg_horsepower=[]
+#___________Horsepower vs. Engine Size________
 hp=data['HP'].unique()
+min=0
 max=hp.max()
 interval=max/8
 interval_temp=interval
-while interval_temp<=max:#loop through interval
-    #loop through all cars within current interval
-    model=data.loc[:,['HP','Price']]
+while interval_temp<=max:
+    print('Sub-Dataframe including '+str(interval_temp-interval)+'to '+str(interval_temp)
+          +' horsepower')
     model=data[data['HP']<interval_temp]
     model=model[model['HP']>interval_temp-interval]
-    model=model[model['Fuel Type']!='Electric']
-    avg_hpp=0 #average price for interval
-    for x in model.iterrorws():#loop through all rows in subdataframe
-        print(x)
-        avg_hpp+=x[1] 
-    avg_hpp=avg_hpp/len(model.index) #average price according to sub frame
-    temp=[]
-    temp.append(interval_temp) 
-    temp.append(avg_hpp)
-    avg_horsepower.append(temp)
+    model=model[model['Fuel Type']!='Electric'] #electric vehicles do not have engine size
+    model=model.sort_values(by='Price')
+    model=model.loc[:,['Model','HP','Engine Size','Price']]
+    model['HP'].plot()
+    model['Engine Size'].plot(secondary_y=True,style='g')
+    matplot.show()
+    print(model)
+    interval_temp+=interval
 
-print("Average horsepower according to ranges:")
-print(avg_horsepower)
